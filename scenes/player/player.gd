aa85de19f7d6a035dd8bd4d2fd669dec70c29116
speregil <s.gilparga@gmail.com>
2025-12-17 13:25:05 +1300

    Added a State machine to control movement and animations
    Added a simple state machine to control the actions of the player

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,13 +4,13 @@ class_name Player extends CharacterBody2D
 ##--------------------------------------------------------------------------------------------------
 ## Fields
 ##--------------------------------------------------------------------------------------------------
-enum State {MOVING, JUMPING, FALLING}
+enum State {MOVING, JUMPING, FALLING}					## Enumeration to control the state machine of the player
 
-@export var top_acceleration: float = 300.0		## Max acceleration on player's movement
-@export var delta_acceleration: float = 15.0	## Change in acceleration on player's movement
-@export var deacceleration_factor: float = 1.5
-@export var midair_acceleration_factor: float = 0.5
-@export var jump_speed: float = 450.0			## Speed for jumping
+@export var top_acceleration: float = 300.0				## Max acceleration on player's movement
+@export var delta_acceleration: float = 15.0			## Change in acceleration on player's movement
+@export var deacceleration_factor: float = 1.5			## Rate at which the player deaccelarates on the floor
+@export var midair_acceleration_factor: float = 0.5		## Rate at which the player deaccelarates midair
+@export var jump_speed: float = 450.0					## Speed for jumping
 
 @onready var animated_sprite = $AnimatedSprite
 
@@ -19,17 +19,17 @@ var direction: float = 0.0				## Movement direction in X
 var acceleration: float = 0.0			## Current acceleration to calculate movement speed
 var acceleration_modifier = 1.0
 var momentum_dir: float = 0.0			## Storages the movement direction before desacelerating
+var can_anim_fall: bool = true
 
 ##--------------------------------------------------------------------------------------------------
 ## Functions
 ##--------------------------------------------------------------------------------------------------
 
 func _process(_delta: float) -> void:
-	print(current_state)
 	_get_input()
+	_animate()
 	_jump()
 	_move()
-	_animate()
 
 ## ## 
 ## Process the movement of the character
@@ -39,14 +39,8 @@ func _move() -> void:
 	velocity.y += GlobalSettings._get_gravity()
 	
 	if current_state == State.JUMPING:
-		if direction == momentum_dir:
-			_accelerate()	
-		elif direction == 0.0:
-			direction = momentum_dir
-			_accelerate()
-		else:
-			velocity.y = 0.0
-			current_state = State.FALLING
+		acceleration_modifier = midair_acceleration_factor
+		_accelerate()
 	elif is_on_floor():
 		current_state = State.MOVING
 		acceleration_modifier = 1.0
@@ -95,7 +89,6 @@ func _jump() -> void:
 	if current_state == State.JUMPING:
 		if is_on_floor():
 			velocity.y = jump_speed * -1
-			animated_sprite.play("Jump")
 		elif velocity.y > 0.0:
 			current_state = State.FALLING
 	elif current_state == State.FALLING:
@@ -112,7 +105,14 @@ func _get_input() -> void:
 		current_state = State.JUMPING
 	
 func _animate() -> void:
-	if is_on_floor():
+	if current_state == State.MOVING:
+		can_anim_fall = true
 		if direction > 0.0: animated_sprite.play("Right")
 		elif direction < 0.0: animated_sprite.play("Left")
 		else: animated_sprite.play("Idle")
+	elif current_state == State.JUMPING:
+		if is_on_floor(): animated_sprite.play("Jump")
+	elif current_state == State.FALLING:
+		if can_anim_fall:
+			animated_sprite.play("Fall")
+			can_anim_fall = false
